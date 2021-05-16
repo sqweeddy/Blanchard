b2208c35f87df04371e168a27df9f618ec7c34e5
sqweeddy <ex1smor@gamil.com>
2021-05-16 22:49:01 +0700

    start galery

--- js/scripts.js
+++ js/scripts.js
@@ -72,6 +72,11 @@ const swiper = new Swiper('.swiper-container', {
   },
   effect: 'fade',
 
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
