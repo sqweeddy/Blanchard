4961d99bb879f95f2be07bcec72487a294dcb935
sqweeddy <ex1smor@gamil.com>
2021-07-13 14:13:56 +0700

    gallery swiper fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,73 @@
 /* Nav */
 
-window.addEventListener('DOMContentLoaded', function() {
-  document.querySelectorAll('.header__link').forEach(function(headerLink){
-    headerLink.addEventListener('click', function() {
-      const path = event.currentTarget.dataset.path
+// window.addEventListener('DOMContentLoaded', function() {
+//   document.querySelectorAll('.header__link').forEach(function(headerLink){
+//     headerLink.addEventListener('click', function() {
+//       const path = event.currentTarget.dataset.path
 
-      document.querySelectorAll('.header__list--dropdown').forEach(function(dropdown) {
-        dropdown.classList.remove('dropdown-list--active')
-      })
+//       document.querySelectorAll('.header__list--dropdown').forEach(function(dropdown) {
+//         dropdown.classList.remove('dropdown-list--active')
+//       })
 
-      document.querySelector(`[data-target="${path}"]`).classList.add('dropdown-list--active')
+//       document.querySelector(`[data-target="${path}"]`).classList.add('dropdown-list--active')
 
-      let activeList = document.querySelector('.dropdown-list--active');
+//       let activeList = document.querySelector('.dropdown-list--active');
 
-      activeList.previousElementSibling('.header__link').classList.add('header__link--active');
-      activeList.closest('.header__img').classList.add('header__img--active');
+//       activeList.previousElementSibling('.header__link').classList.add('header__link--active');
+//       activeList.closest('.header__img').classList.add('header__img--active');
 
       // document.querySelectorAll('.dropdown-list--active > .header__img').classList.toggle('header__img--active');
       // document.querySelectorAll('.dropdown-list--active > .header__link').classList.toggle('header__link--active');
-    })
-  })
-})
+//     })
+//   })
+// })
+
+const params = {
+  btnClassName: "header__link",
+  activeClassName: "dropdown-list--active",
+  disabledClassName: "dropdown-list--disable"
+}
+
+function onDisable(evt) {
+  if (evt.target.classList.contains(params.disabledClassName)) {
+    evt.target.classList.remove(params.disabledClassName, params.activeClassName);
+    evt.target.removeEventListener("animationend", onDisable);
+  }
+}
+
+function setMenuListener() {
+  document.body.addEventListener("click", (evt) => {
+    const activeElements = document.querySelectorAll(`.${params.activeClassName}`);
+
+    if (activeElements.length && !evt.target.closest(`.${params.activeClassName}`)) {
+      activeElements.forEach((current) => {
+        if (current.classList.contains(params.btnClassName)) {
+          current.classList.remove(params.activeClassName);
+        } else {
+          current.classList.add(params.disabledClassName);
+        }
+      });
+    }
+
+    if (evt.target.closest(`.${params.btnClassName}`)) {
+      const btn = evt.target.closest(`.${params.btnClassName}`);
+      const path = btn.dataset.path;
+      const drop = document.querySelector(`[data-target="${path}"]`);
+
+      btn.classList.toggle(params.activeClassName);
+
+      if (!drop.classList.contains(params.activeClassName)) {
+        drop.classList.add(params.activeClassName);
+        drop.addEventListener("animationend", onDisable);
+      } else {
+        drop.classList.add(params.disabledClassName);
+      }
+    }
+  });
+}
+
+setMenuListener();
+
 
 
 /* Menu */
@@ -93,14 +140,11 @@ const choices = new Choices(element, {
 /* Swiper */
 
 var swiper = new Swiper('.gallery__swiper', {
-  width: 1100,
-  slidesPerView: 3,
-  slidesPerColumn: 2,
-  slidesPerGroup: 3,
-  // effect: 'fade',
-  // autoplay: {
-  //   delay: 5000,
-  // },
+  // slidesPerColumnFill: 'row',
+  // slidesPerView: 3,
+  // slidesPerColumn: 2,
+  // slidesPerGroup: 3,
+  // spaceBetween: 50,
   navigation: {
     nextEl: '.swiper-button-right',
     prevEl: '.swiper-button-left',
@@ -110,4 +154,38 @@ var swiper = new Swiper('.gallery__swiper', {
     type: 'fraction',
     clickable: true,
   },
+
+  breakpoints: {
+    320: {
+      slidesPerColumnFill: 'row',
+      slidesPerView: 1,
+      slidesPerColumn: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 1,
+    },
+
+    768: {
+      slidesPerColumnFill: 'row',
+      slidesPerView: 2,
+      slidesPerColumn: 2,
+      slidesPerGroup: 4,
+      spaceBetween: 34,
+    },
+
+    1024: {
+      slidesPerColumnFill: 'row',
+      slidesPerView: 2,
+      slidesPerColumn: 2,
+      slidesPerGroup: 4,
+      spaceBetween: 34,
+    },
+
+    1700: {
+      slidesPerColumnFill: 'row',
+      slidesPerView: 3,
+      slidesPerColumn: 2,
+      slidesPerGroup: 3,
+      spaceBetween: 50,
+    },
+  }
 });
