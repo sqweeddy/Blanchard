bfc73a1c7e83f54d03bbda81449be7ca86d96bb9
sqweeddy <ex1smor@gamil.com>
2021-07-19 15:21:56 +0700

    new gallery

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,5 @@
 /* Nav */
 
-// window.addEventListener('DOMContentLoaded', function() {
-//   document.querySelectorAll('.header__link').forEach(function(headerLink){
-//     headerLink.addEventListener('click', function() {
-//       const path = event.currentTarget.dataset.path
-
-//       document.querySelectorAll('.header__list--dropdown').forEach(function(dropdown) {
-//         dropdown.classList.remove('dropdown-list--active')
-//       })
-
-//       document.querySelector(`[data-target="${path}"]`).classList.add('dropdown-list--active')
-
-//       let activeList = document.querySelector('.dropdown-list--active');
-
-//       activeList.previousElementSibling('.header__link').classList.add('header__link--active');
-//       activeList.closest('.header__img').classList.add('header__img--active');
-
-      // document.querySelectorAll('.dropdown-list--active > .header__img').classList.toggle('header__img--active');
-      // document.querySelectorAll('.dropdown-list--active > .header__link').classList.toggle('header__link--active');
-//     })
-//   })
-// })
-
 const params = {
   btnClassName: "header__link",
   activeClassName: "is-active",
@@ -70,43 +48,40 @@ setMenuListener();
 
 
 
-/* Menu */
 
 window.addEventListener('DOMContentLoaded', function() {
+  /* Menu */
+
   document.querySelector('#burger').addEventListener('click', function() {
     document.querySelector('#menu').classList.toggle('menu--is-active');
-  })
-})
+  });
 
-window.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#close').addEventListener('click', function() {
     document.querySelector('#menu').classList.toggle('menu--is-active');
-  })
-})
+  });
+  /* Search */
 
-/* Search */
-
-window.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#search').addEventListener('click', function() {
     document.querySelector('.search-top').classList.toggle('search-top--active');
     document.querySelector('#close-search').classList.toggle('search__close--active');
     document.querySelector('#searchInput').classList.toggle('search-top__input--is-active');
-  })
-})
+  });
 
-window.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#close-search').addEventListener('click', function() {
     document.querySelector('.search-top').classList.toggle('search-top--active');
     document.querySelector('#close-search').classList.toggle('search__close--active');
     document.querySelector('#searchInput').classList.toggle('search-top__input--is-active');
-  })
+  });
 
   document.querySelector('.hero').addEventListener('click', function() {
     document.querySelector('.search-top').classList.remove('search-top--active');
     document.querySelector('#close-search').classList.remove('search__close--active');
     document.querySelector('#searchInput').classList.remove('search-top__input--is-active');
-  })
-})
+  });
+
+
+});
+
 
 /* Select */
 
@@ -140,11 +115,6 @@ const choices = new Choices(element, {
 /* Swiper */
 
 var swiper = new Swiper('.gallery__swiper', {
-  // slidesPerColumnFill: 'row',
-  // slidesPerView: 3,
-  // slidesPerColumn: 2,
-  // slidesPerGroup: 3,
-  // spaceBetween: 50,
   navigation: {
     nextEl: '.swiper-button-right',
     prevEl: '.swiper-button-left',
@@ -180,12 +150,48 @@ var swiper = new Swiper('.gallery__swiper', {
       spaceBetween: 34,
     },
 
-    1700: {
+    1400: {
       slidesPerColumnFill: 'row',
       slidesPerView: 3,
       slidesPerColumn: 2,
       slidesPerGroup: 3,
       spaceBetween: 50,
     },
-  }
+  },
+});
+
+/* Popup */
+
+$(document).ready(function() {
+	$('.popup-with-zoom-anim').magnificPopup({
+		type: 'inline',
+
+		fixedContentPos: false,
+		fixedBgPos: true,
+
+		overflowY: 'auto',
+
+		closeBtnInside: true,
+		preloader: false,
+
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-zoom-in'
+	});
+
+	$('.popup-with-move-anim').magnificPopup({
+		type: 'inline',
+
+		fixedContentPos: false,
+		fixedBgPos: true,
+
+		overflowY: 'auto',
+
+		closeBtnInside: true,
+		preloader: false,
+
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-slide-bottom'
+	});
 });
